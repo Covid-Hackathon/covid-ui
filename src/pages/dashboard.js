6ec73fddc1eda0dd420a49d7599f2bc646c83012
Alfonso Javier Prado Sepúlveda <alfonso.prado.s@gmail.com>
2020-08-11 23:05:30 -0400

    Add plotly

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Line } from 'react-chartjs-2';
+
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
-
 import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
@@ -19,8 +18,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import api from '../api';
 import Map from '../components/map';
 import TableDays from '../components/table-days';
-import TableWeeks from '../components/table-weeks';
-import Loading from '../components/loading'
+import Loading from '../components/loading';
+import Plot from '../components/plot';
+
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
@@ -36,131 +36,6 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(2, 4, 3),
   },
 }));
-
-const activeChart = (place, realData, predictionData, property, name, colorPast, colorForecast) => {
-  function scaleYaxis(max_elem) {
-    var dig = 0
-    if (max_elem < 10)
-        return 15
-    while (max_elem > 9) {
-        dig += 1
-        max_elem /= 10
-    }
-    if (dig < 3) {
-
-        return ((max_elem + 2) * Math.pow(10, dig))
-    }
-    else {
-        return ((max_elem + 1) * Math.pow(10, dig))
-    }
-  }
-
-  const prev_dates = realData.map(item => item.date);
-  const next_dates = predictionData.map(item => item.date);
-  const prev_active = realData.map(item => item[property]);
-  const next_active = predictionData.map(item => item[property]);
-
-  const p_len = prev_active.length - 1;
-  next_active.unshift(prev_active[p_len]);
-  let max_act = prev_active[p_len];
-
-  for (let i = 0; i < p_len; i++) {
-    next_active.unshift(null);
-
-    if (prev_active[i] > max_act) {
-        max_act = prev_active[i]
-    }
-  }
-
-  for (let i = 0; i < next_dates.length; i++) {
-      prev_active.push(null);
-
-      if (next_active[i] > max_act) {
-          max_act = next_active[i]
-      }
-  }
-
-  const dates = [
-    ...prev_dates,
-    ...next_dates
-  ];
-
-  const yaxis_scale = scaleYaxis(max_act);
-
-  const data = {
-      labels: dates,
-      datasets: [{
-          label: name,
-          fill: false,
-          lineTension: 0.2,
-          borderColor: colorPast,
-          borderCapStyle: 'square',
-          borderJoinStyle: 'miter',
-          pointBackgroundColor: "black",
-          pointBorderWidth: 0,
-          pointHoverRadius: 8,
-          pointHoverBackgroundColor: "black",
-          pointHoverBorderWidth: 0,
-          pointRadius: 3,
-          pointHitRadius: 10,
-          data: prev_active,
-          spanGaps: true,
-      },
-      {
-          label: "Forecast",
-          fill: false,
-          lineTension: 0.3,
-          borderColor: colorForecast,
-          borderCapStyle: 'square',
-          borderJoinStyle: 'miter',
-          pointBackgroundColor: "black",
-          pointBorderWidth: 0,
-          pointHoverRadius: 8,
-          pointHoverBackgroundColor: "black",
-          pointHoverBorderWidth: 0,
-          pointRadius: 3,
-          pointHitRadius: 10,
-          data: next_active,
-          spanGaps: true,
-      }]
-  };
-
-  const options = {
-      title: {
-          display: true,
-          text: `${place} ${name} cases Forecast(next 21 days)`
-      },
-      maintainAspectRatio: false,
-      responsive: true,
-      scales: {
-          yAxes: [{
-              ticks: {
-                  beginAtZero: true,
-                  suggestedMax: yaxis_scale,
-                  maxTicksLimit: 9
-              }
-          }],
-          xAxes: [{
-              ticks: {
-                  autoSkip: false
-              }
-          }]
-      },
-      legend: {
-          display: true,
-          position: 'bottom'
-      },
-      layout: {
-          padding: 30
-      }
-  };
-  
-  return {
-    data,
-    options
-  }
-}
-
 
 const Dashboard = () => {
   const classes = useStyles();
@@ -382,11 +257,6 @@ const Dashboard = () => {
     }
     return country
   }
-
-  const lineActive = activeChart(title(), pastData, predictionData, 'active', 'Active', 'blue', 'orange');
-  const lineConfirmed = activeChart(title(), pastData, predictionData, 'confirmed', 'Confirmed', 'red', 'darkgoldenrod');
-  const lineDeaths = activeChart(title(), pastData, predictionData, 'deaths', 'Deceased', 'gray', 'saddlebrown');
-
   return (
     <>
       <Container maxWidth="xl" component="main">
@@ -493,16 +363,38 @@ const Dashboard = () => {
                   <Grid item style={{height: '400px', padding: '5px'}} xs={12}>
                     {
                       plot === 'Active' &&
-                      <Line data={lineActive.data} options={lineActive.options} />
+                      <Plot 
+                        title={`${title()} Active cases Forecast (next 21 days)`}
+                        type="active"
+                        pastColor="blue" 
+                        predictionColor="orange" 
+                        pastData={pastData} 
+                        predictionData={predictionData} 
+                      />
                     }
                     {
                       plot === 'Confirmed' &&
-                      <Line data={lineConfirmed.data} options={lineConfirmed.options} />
+                      <Plot 
+                        title={`${title()} Confirmed cases Forecast (next 21 days)`}
+                        type="confirmed"
+                        pastColor="blue" 
+                        predictionColor="orange" 
+                        pastData={pastData} 
+                        predictionData={predictionData} 
+                      />
                     }
                     {
                       plot === 'Deceased' &&
-                      <Line data={lineDeaths.data} options={lineDeaths.options}/>
+                      <Plot 
+                        title={`${title()} Deceased cases Forecast (next 21 days)`}
+                        type="deaths"
+                        pastColor="blue" 
+                        predictionColor="orange" 
+                        pastData={pastData} 
+                        predictionData={predictionData} 
+                      />
                     }
+
                   </Grid>
                   <Grid item xs={12} style={{padding: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
                       <ButtonGroup size="large">
