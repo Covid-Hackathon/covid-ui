fd74ea0252e6f76db99da854dadf72db2cb8e738
Alfonso Javier Prado Sepúlveda <alfonso.prado.s@gmail.com>
2020-08-20 21:51:55 -0400

    Confirmed, Active, Recovered, Deceased

--- src/pages/historical-projections.js
+++ src/pages/historical-projections.js
@@ -60,7 +60,7 @@ const HistoricalProjections = () => {
 
     const [ , setSearchBarRegion ] = useState(null); 
 
-    const [ plot, setPlot ] = useState('Active');
+    const [ plot, setPlot ] = useState('Confirmed');
 
     const [ notContent, setNotContent ] = useState(false);
 
@@ -252,9 +252,9 @@ const HistoricalProjections = () => {
                         </Grid>
                         <Grid item xs={12} style={{padding: '20px', paddingTop:'40px', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
                             <ButtonGroup size="large">
+                                <Button variant={plot === 'Confirmed'  ? "contained" : "outlined"} color={plot === 'Confirmed' ? "primary" : "default"} onClick={plotHandler.bind(this, 'Confirmed')}>Confirmed</Button>
                                 <Button variant={plot === 'Active'  ? "contained" : "outlined"} color={plot === 'Active' ? "primary" : "default"} onClick={plotHandler.bind(this, 'Active')}>Active</Button>
                                 <Button variant={plot === 'Recovered'  ? "recovered" : "outlined"} color={plot === 'Recovered' ? "primary" : "default"} onClick={plotHandler.bind(this, 'Recovered')}>Recovered</Button>
-                                <Button variant={plot === 'Confirmed'  ? "contained" : "outlined"} color={plot === 'Confirmed' ? "primary" : "default"} onClick={plotHandler.bind(this, 'Confirmed')}>Confirmed</Button>
                                 <Button variant={plot === 'Deceased'  ? "contained" : "outlined"} color={plot === 'Deceased' ? "primary" : "default"} onClick={plotHandler.bind(this, 'Deceased')}>Deceased</Button>
                             </ButtonGroup>
                         </Grid>
