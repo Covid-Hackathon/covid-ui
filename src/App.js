29c2828eca1af00803411b4d76d0b021946c22f9
Taarak Rapolu <63356126+Taarak9@users.noreply.github.com>
2020-08-21 13:44:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -69,9 +69,6 @@ const App = () => {
               <Link to="/dashboard" className={classes.link}>
                 Dashboard
               </Link>
-              <Link to="/about-the-model" className={classes.link}>
-                About the Model
-              </Link>
               <Link to="/historical-projections" className={classes.link}>
                 Historical Projections
               </Link> 
@@ -91,14 +88,14 @@ const App = () => {
           <Route path='/dashboard' exact>
             <Dashboard />
           </Route>
-          <Route path='/about-the-model' exact>
-            <AboutTheModel />
-          </Route>
           <Route path='/historical-projections' exact>
             <HistoricalProjections />
           </Route>
           <Route path='/medical-inventory' exact>
             <MedicalInventory />
+          </Route>
+          <Route path='/about-the-model' exact>
+            <AboutTheModel />
           </Route>
           <Route path='/meet-the-team' exact>
             <MeetTheTeam />
